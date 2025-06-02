6e9800d368781bf42cf277d4087edd3803ac56c0
fahim017272 <botfahim232@gmail.com>
2025-06-02 18:22:11 +0600

    Update join.js

--- scripts/events/join.js
+++ scripts/events/join.js
@@ -106,11 +106,11 @@ https://www.facebook.com/hackar.vaiya
         let avtAnime = (await axios.get(encodeURI(
           `https://graph.facebook.com/${event.logMessageData.addedParticipants[o].userFbId}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`), { responseType: "arraybuffer" })).data;
         var ok = [
-          'https://i.imgur.com/dDSh0wc.jpeg',
-          'https://i.imgur.com/UucSRWJ.jpeg',
-          'https://i.imgur.com/OYzHKNE.jpeg',
-          'https://i.imgur.com/V5L9dPi.jpeg',
-          'https://i.imgur.com/M7HEAMA.jpeg'
+          'https://i.imgur.com/u5WPb1q.mp4',
+          'https://i.imgur.com/XcSE5Vj.mp4',
+          'https://i.imgur.com/MEGpuIm.mp4',
+          'https://i.imgur.com/0frNz8C.jpeg',
+          'https://i.imgur.com/EY7l8H3.jpeg'
         ]
         let background = await new Promise((resolve, reject) => {
           request.get(
